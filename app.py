351b965a1ee0d7bf5e3b84f97d5a85a99d565082
이승렬 <ibban9810@icloud.com>
2023-01-16 15:01:51 +0900

    add: name_init, answer_post add

--- app.py
+++ app.py
@@ -24,11 +24,16 @@ client = MongoClient(
 db = client.dbsparta
 
 img_result_dic = {
-    '1': '-url',
-    '2': '-url',
-    '3': '-url',
-    '4': '-url',
-    '5': '-url'
+    '1': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon01.png',
+    '2': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon02.png',
+    '3': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon03.png',
+    '4': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon04.png',
+    '5': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon05.png',
+    '6': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon06.png',
+    '7': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon07.png',
+    '8': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon08.png',
+    '9': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon09.png',
+    '10': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon10.png'
 }
 
 
@@ -40,11 +45,17 @@ def home():
 ## 닉네임 저장
 @app.route("/api/name", methods=["POST"])
 def name_init():
-    # 닉네임을 받아옵니다.
+
+    # user 의 닉네임을 받아옵니다.
     name_receive = request.form['name_give']
-    ## result 값을 숫자로 받음 -> 숫자를 통해서 사진 url 받아옴.
+
+    ## Result 값 0으로 init
     result_receive = 0;
+
+    ## url 값 임의의 url 값으로 init
     url_receive = 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon01.png'
+
+    ## result_list 값 0으로 init
     result_list_receive = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     doc = {
@@ -55,29 +66,50 @@ def name_init():
     }
 
     db.users.insert_one(doc)
+
     return jsonify({'msg': '닉네임 등록 완료!'})
 
 
 ## 결과값을 질문지 하나하나마다 할때 post API 실행
 @app.route("/api/answer", methods=["POST"])
 def answer_post():
-    ## 1줬어요
+
     answer_receive = request.form['answer_give']
     name_receive = request.form['name_give']
 
-    ## name_receive == db에서 이름이 같은거를 뽑아여
-    ##그 걸 뽑은거에 result_list의 값을 업데이트하는 것
+    users_list = list(db.users.find({}, {'_id': False}))
+    result_list = users_list.get('result_list')
 
-    if (answer_receive == 1):
-    ### 증감을 써줄것
+    for user in users_list:
+        if (name_receive == user.get('name')):
+            if (answer_receive == 1):
+                result_list[0] += 1;
+                result_list[1] += 1;
+                result_list[2] += 1;
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[0]': result_list[0] }})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[1]': result_list[1] }})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[2]': result_list[2] }})
 
-    if (answer_receive == 2):
-    ### 증감을 써줄것
+            if (answer_receive == 2):
+                result_list[3] += 1;
+                result_list[4] += 1;
+                result_list[5] += 1;
+                result_list[6] += 1;
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[3]': result_list[3]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[4]': result_list[4]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[5]': result_list[5]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[6]': result_list[6]}})
 
-    if (answer_receive == 3):
-    ### 증감을 써줄것
-=
-    # return jsonify({'msg': '닉네임 등록 완료!'})
+            if (answer_receive == 3):
+                result_list[7] += 1;
+                result_list[8] += 1;
+                result_list[9] += 1;
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[7]': result_list[7]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[8]': result_list[8]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[9]': result_list[9]}})
+
+
+# return jsonify({'msg': '닉네임 등록 완료!'})
 
 
 ## 결과물 보여주기
