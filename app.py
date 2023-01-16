10d489522e3afe666c39a2623039807712e31845
이승렬 <ibban9810@icloud.com>
2023-01-16 22:58:08 +0900

    Fix: func code 간편화

--- app.py
+++ app.py
@@ -25,13 +25,13 @@ db = client.dbsparta
 
 img_result_dic = {
     '1': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon01.png',
-    '2': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon02.png',
-    '3': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon03.png',
-    '4': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon04.png',
-    '5': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon05.png',
-    '6': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon06.png',
-    '7': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon07.png',
-    '8': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon08.png',
+    '2': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon06.png',
+    '3': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon04.png',
+    '4': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon05.png',
+    '5': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon07.png',
+    '6': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon08.png',
+    '7': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon02.png',
+    '8': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon03.png',
     '9': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon09.png',
     '10': 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon10.png'
 }
@@ -45,19 +45,20 @@ def home():
 ## 닉네임 저장
 @app.route("/api/name", methods=["POST"])
 def name_init():
-
     # user 의 닉네임을 받아옵니다.
     name_receive = request.form['name_give']
+
+    # user의 닉네임이 중복인지 확인합니다.
     users_list = list(db.users.find({}, {'_id': False}))
     for user in users_list:
-        if (name_receive == user.get('name')):
+        if name_receive == user.get('name'):
             return jsonify({'msg': '중복입니다!'})
 
     ## Result 값 0으로 init
     result_receive = 0;
 
     ## url 값 임의의 url 값으로 init
-    url_receive = 'http://spartacodingclub.shop/static/images/rtans/SpartaIcon01.png'
+    url_receive = ''
 
     ## result_list 값 0으로 init
     result_list_receive = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -77,44 +78,33 @@ def name_init():
 ## 결과값을 질문지 하나하나마다 할때 post API 실행
 @app.route("/api/answer", methods=["POST"])
 def answer_post():
-
+    # 숫자 하나만 넘겨주는 경우
     answer_receive = request.form['answer_give']
     name_receive = request.form['name_give']
 
     users_list = list(db.users.find({}, {'_id': False}))
-    result_list = users_list.get('result_list')
+    # # result_list = users_list.get('result_list')
+    result_list = db.users.find_one({'name': name_receive}, {'_id': False}, {'result': False}, {'image': False},
+                                    {'name': False})
+    # for user in users_list:
+    if answer_receive == 1:
+        result_list[0] += 1;
+        result_list[3] += 1;
+        result_list[5] += 1;
+        db.users.update_one({'name': name_receive}, {'$set': {'result_list': result_list}})
 
-    for user in users_list:
-        if (name_receive == user.get('name')):
-            if (answer_receive == 1):
-                result_list[0] += 1;
-                result_list[3] += 1;
-                result_list[5] += 1;
-                # db.users.update_one({'name': name_receive}, {'$set': {'result_list': result_list}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[0]': result_list[0]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[3]': result_list[3]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[5]': result_list[5]}})
+    if answer_receive == 2:
+        result_list[4] += 1;
+        result_list[6] += 1;
+        result_list[7] += 1;
+        db.users.update_one({'name': name_receive}, {'$set': {'result_list': result_list}})
 
-            if (answer_receive == 2):
-                result_list[4] += 1;
-                result_list[6] += 1;
-                result_list[7] += 1;
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[4]': result_list[4]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[6]': result_list[6]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[7]': result_list[7]}})
-
-            if (answer_receive == 3):
-                result_list[1] += 1;
-                result_list[2] += 1;
-                result_list[8] += 1;
-                result_list[9] += 1;
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[1]': result_list[1]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[2]': result_list[2]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[8]': result_list[8]}})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[9]': result_list[9]}})
-
-
-# return jsonify({'msg': '닉네임 등록 완료!'})
+    if answer_receive == 3:
+        result_list[1] += 1;
+        result_list[2] += 1;
+        result_list[8] += 1;
+        result_list[9] += 1;
+        db.users.update_one({'name': name_receive}, {'$set': {'result_list': result_list}})
 
 
 ## 결과물 보여주기
@@ -125,18 +115,20 @@ def result_update():
     users_list = list(db.users.find({}, {'_id': False}))
     result_list = users_list.get('result_list')
 
+    ## result Update
     for user in users_list:
         if name_receive == user.get('name'):
             db.users.update_one({'name': name_receive}, {'$set': {'result': result_list[max]}})
 
-    ## image Url 도 이에 따라서 바꿔주기
-    url_list = img_result_dic[result_list[max]]
+    ## image Url Update
+    url_list = img_result_dic['result_list[max]']
     db.users.update_one({'name': name_receive}, {'$set': {'image': url_list}})
 
-    for user in users_list:
-        if name_receive == user.get('name'):
-            img_url = users_list.get('image')
-
+    # for user in users_list:
+    #     if name_receive == user.get('name'):
+    #         img_url = users_list.get('image')
+    # 다음과 같이 for 문을 쓰는 것 대신에 find_one을 사용함.
+    img_url = db.users.find_one({'name': name_receive}, {'_id': False}, {'result': False}, {'result_list': False}, {'name': False})
     return jsonify({'img_url': img_url})
 
 
@@ -145,12 +137,11 @@ def result_update():
 def result_get():
     name_receive = request.form['name_give']
     users_list = list(db.users.find({}, {'_id': False}))
-    for user in users_list:
-        if name_receive == user.get('name'):
-            img_url = users_list.get('image')
-
+    # for user in users_list:
+    #     if name_receive == user.get('name'):
+    #         img_url = users_list.get('image')
+    img_url = db.users.find_one({'name': name_receive}, {'_id': False}, {'result': False}, {'result_list': False}, {'name': False})
     return jsonify({'img_url': img_url})
-
 
 
 if __name__ == '__main__':
