adb8ee0a5637fac0e959e46121d0c996cb428eb7
이승렬 <ibban9810@icloud.com>
2023-01-16 16:26:45 +0900

    fix: result_get, result_update 함수명 변경및 수정

--- app.py
+++ app.py
@@ -48,6 +48,10 @@ def name_init():
 
     # user 의 닉네임을 받아옵니다.
     name_receive = request.form['name_give']
+    users_list = list(db.users.find({}, {'_id': False}))
+    for user in users_list:
+        if (name_receive == user.get('name')):
+            return jsonify({'msg': '중복입니다!'})
 
     ## Result 값 0으로 init
     result_receive = 0;
@@ -86,9 +90,10 @@ def answer_post():
                 result_list[0] += 1;
                 result_list[1] += 1;
                 result_list[2] += 1;
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[0]': result_list[0] }})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[1]': result_list[1] }})
-                db.users.update_one({'name': name_receive}, {'$set': {'result_list[2]': result_list[2] }})
+                # db.users.update_one({'name': name_receive}, {'$set': {'result_list': result_list}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[0]': result_list[0]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[1]': result_list[1]}})
+                db.users.update_one({'name': name_receive}, {'$set': {'result_list[2]': result_list[2]}})
 
             if (answer_receive == 2):
                 result_list[3] += 1;
@@ -113,31 +118,43 @@ def answer_post():
 
 
 ## 결과물 보여주기
-@app.route("/api/result", methods=["GET"])
-def result_get():
-    ## result 의 값을 0에서  최댓값으로 바꾸기
-    ## image Url 도 이에 따라서 바꿔주기
-    ## db update 하기
+@app.route("/api/result", methods=["POST"])
+def result_update():
+    ## result 의 값을 0에서  최댓값 (result_high) 으로 바꾸기
+    name_receive = request.form['name_give']
     users_list = list(db.users.find({}, {'_id': False}))
-    return jsonify({'users_list': users_list})
+    result_list = users_list.get('result_list')
+
+    tmp = 0;
+    for result in result_list:
+        if(tmp >= result):
+            tmp = result
+
+    result_high = tmp
+    db.users.update_one({'name': name_receive}, {'$set': {'result': result_high}})
+
+    ## image Url 도 이에 따라서 바꿔주기
+    url_update = img_result_dic[result_high]
+    db.users.update_one({'name': name_receive}, {'$set': {'image': url_update}})
+
+    for user in users_list:
+        if (name_receive == user.get('name')):
+            img_url = users_list.get('image')
+
+    return jsonify({'img_url': img_url})
 
 
 ## 내 결과 다시보기
-@app.route("/api/result/name?", methods=["GET"])
-def re_result_get():
+@app.route("/api/re_result", methods=["POST"])
+def result_get():
     name_receive = request.form['name_give']
+    users_list = list(db.users.find({}, {'_id': False}))
+    for user in users_list:
+        if (name_receive == user.get('name')):
+            img_url = users_list.get('image')
 
-    ## db에서 이름이 같은 친구를 db에서 찾아서 이에 대한 리스트를 보내주면 된다.
+    return jsonify({'img_url': img_url})
 
-    veggie_list = list(db.veggie.find({}, {'_id': False}))
-    return jsonify({'veggie': veggie_list})
-
-
-## 르탄이 이미지 가져오기
-@app.route("/api/img", methods=["GET"])
-def img_get():
-    veggie_list = list(db.veggie.find({}, {'_id': False}))
-    return jsonify({'veggie': veggie_list})
 
 
 if __name__ == '__main__':
